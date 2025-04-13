7aab29c3745c03b8ed4ffa539cec3aea3e882107
ALI <alirezaei.eng7@gmail.com>
2025-04-13 21:13:31 +0330

    add constants

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div>
-      <Button className="caret-amber-100">hi</Button>
-    </div>
-  );
-}
